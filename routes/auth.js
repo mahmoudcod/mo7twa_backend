3001d7e8bfc644842535d496d99ac77153d55daf
mahmoud kamel <spop66009@gmail.com>
2024-10-09 15:21:15 +0300

    make the token time more longer

--- routes/auth.js
+++ routes/auth.js
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
         res.json({ token, user: { id: user._id, email: user.email, country: user.country, isAdmin: user.isAdmin, isConfirmed: user.isConfirmed } });
     } catch (error) {
         res.status(500).json({ message: 'Error logging in', error: error.message });
