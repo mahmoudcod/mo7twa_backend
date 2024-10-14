df0ab1b5005d582212b4398fe7b858cd0f814b46
mahmoud kamel <spop66009@gmail.com>
2024-10-14 17:41:19 +0300

    fix the category error in the page model

--- models/page.js
+++ models/page.js
@@ -10,7 +10,7 @@ const pageSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    category: [{
+    categories: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category'
     }],
