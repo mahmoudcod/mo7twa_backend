2c6310298cb21fa2d525dd685250fb4cb380ae31
mahmoud kamel <spop66009@gmail.com>
2024-10-14 13:38:13 +0300

    edit the category model to add the pages

--- models/category.js
+++ models/category.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const CategorySchema = new mongoose.Schema({
     name: { type: String, required: true },
     subcategories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Subcategory' }], // List of subcategories
+    pages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Page' }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Category', CategorySchema);
